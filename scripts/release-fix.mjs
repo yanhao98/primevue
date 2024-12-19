845508779c4ada32473e96f5487aa8d2fd7b9fc5
严浩 <me@yanhao.ren>
2024-12-19 15:47:34 +0800

    release-fix.mjs

---
+++ scripts/release-fix.mjs
@@ -0,0 +1,78 @@
+import { execSync } from 'child_process';
+import fs, { writeFileSync } from 'fs';
+import pkg from '../package.json' with { type: 'json' };
+
+let newVersion = execSync('npm version patch --no-git-tag-version', { encoding: 'utf-8' });
+
+console.debug(`newVersion :>> `, newVersion);
+
+newVersion = newVersion.replace(/^v/, '').trim() + '-temp-fix';
+const tgzName = `primevue-${newVersion}.tgz`;
+
+console.debug(`newVersion :>> `, newVersion);
+console.debug(`tgzName :>> `, tgzName);
+
+pkg.version = newVersion;
+writeFileSync('package.json', JSON.stringify(pkg, null, 2));
+
+function build() {
+    let buildCommand = `pnpm --filter primevue build`;
+
+    console.debug(`buildCommand :>> `, buildCommand);
+    execSync(buildCommand, { stdio: 'inherit' });
+}
+
+function publish() {
+    let publishCommand = `pnpm publish --filter primevue --no-git-checks --access public`;
+
+    publishCommand += ' --tag latest';
+    publishCommand += ' --dry-run';
+    publishCommand += ' --registry https://nexus.oo1.dev/repository/npm-hosted/';
+    console.debug(`publishCommand :>> `, publishCommand);
+    execSync(publishCommand, { stdio: 'inherit' });
+}
+
+function pack() {
+    // https://nexus.oo1.dev/#browse/upload:npm-hosted
+
+    let packCommand = `npm pack`;
+
+    console.debug(`packCommand :>> `, packCommand);
+    execSync(packCommand, { stdio: 'inherit', cwd: process.cwd() + '/packages/primevue/dist' });
+}
+
+function upload() {
+    const myHeaders = new Headers();
+
+    myHeaders.append('Authorization', `Basic ${process.env.NEXUS_AUTH}`);
+
+    const formdata = new FormData();
+
+    formdata.append('npm.asset', new File([fs.readFileSync(`packages/primevue/dist/${tgzName}`)], tgzName));
+
+    const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: formdata,
+        redirect: 'follow'
+    };
+
+    fetch('https://nexus.oo1.dev/service/rest/v1/components?repository=npm-hosted', requestOptions).then((response) => console.debug(`response :>> `, response));
+}
+
+function list() {
+    fetch('https://nexus.oo1.dev/service/rest/v1/components?repository=npm-hosted', {
+        method: 'GET'
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.debug(`data :>> `, data);
+        });
+}
+
+build();
+// publish();
+pack();
+upload();
